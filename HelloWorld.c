116ab30a8d31412b2d9e9e6a53a8911f9e788d6c
pablo0910 <pablo0910@users.noreply.github.com>
2015-01-19 12:41:10 +0100

    Hello World

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#ifdef _WIN32
+
 int main(void) {
 
 	printf("\nHola Mundo! [.c]\n");
+	system("PAUSE"); //Windows only
+	return 0;
 
 }
+
+#else
+
+int main(void) {
+
+	printf("\nHola Mundo! [.c]\n");
+	return 0;
+
+}
+
+#endif
